733849abbeee9699e2986e660f52212ee754372d
jake2191 <jake2191@live.com.au>
2026-07-21 18:55:41 +1000

    Delete RemoteSliderControl.lrplugin directory

--- RemoteSliderControl.lrplugin/Init.lua
+++
@@ -1,5 +0,0 @@
--- Runs automatically when the plugin loads. Keeps a single shared
--- RemoteServer instance alive for the life of the catalog session.
-
-_G.RemoteSliderControlServer = require 'RemoteServer'
-_G.RemoteSliderControlServer.start()
